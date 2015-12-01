c3c262651c9f7cdff292abb1ac3ce7fd6b94c2d2
Nate Armstrong <natearmstrong2@gmail.com>
2015-11-30 19:59:23 -0700

    Add terms and button

--- AutoLayoutTutorial/ViewController.swift
+++ AutoLayoutTutorial/ViewController.swift
@@ -12,14 +12,79 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        // terms
+        let terms = UILabel()
+        terms.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(terms)
+        terms.text = "By joining you agree to our Terms & Privacy Policy"
+        terms.font = UIFont.systemFontOfSize(12)
+        terms.textColor = UIColor.lightGrayColor()
+        terms.textAlignment = .Center
+        view.addConstraint(NSLayoutConstraint(
+            item: terms,
+            attribute: .Bottom,
+            relatedBy: .Equal,
+            toItem: view,
+            attribute: .BottomMargin,
+            multiplier: 1,
+            constant: -8))
+        view.addConstraint(NSLayoutConstraint(
+            item: terms,
+            attribute: .Leading,
+            relatedBy: .Equal,
+            toItem: view,
+            attribute: .LeadingMargin,
+            multiplier: 1,
+            constant: 0))
+        view.addConstraint(NSLayoutConstraint(
+            item: terms,
+            attribute: .Trailing,
+            relatedBy: .Equal,
+            toItem: view,
+            attribute: .TrailingMargin,
+            multiplier: 1,
+            constant: 0))
 
+        // submit button
+        let button = UIButton(type: .System)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        button.backgroundColor = UIColor.darkGrayColor()
+        button.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        button.setTitle("JOIN SPRING", forState: .Normal)
+        view.addConstraint(NSLayoutConstraint(
+            item: button,
+            attribute: .Bottom,
+            relatedBy: .Equal,
+            toItem: terms,
+            attribute: .Top,
+            multiplier: 1,
+            constant: -20))
+        view.addConstraint(NSLayoutConstraint(
+            item: button,
+            attribute: .Leading,
+            relatedBy: .Equal,
+            toItem: view,
+            attribute: .LeadingMargin,
+            multiplier: 1,
+            constant: 0))
+        view.addConstraint(NSLayoutConstraint(
+            item: button,
+            attribute: .Trailing,
+            relatedBy: .Equal,
+            toItem: view,
+            attribute: .TrailingMargin,
+            multiplier: 1,
+            constant: 0))
+        button.addConstraint(NSLayoutConstraint(
+            item: button,
+            attribute: .Height,
+            relatedBy: .Equal,
+            toItem: nil,
+            attribute: .NotAnAttribute,
+            multiplier: 1,
+            constant: 50))
+    }
 
 }
-
